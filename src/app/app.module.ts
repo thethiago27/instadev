1089bd0626d91a0901232248cbe68d2f203e885e
thethiago27 <pentazyeclipse@gmail.com>
2020-08-27 12:54:50 -0400

    Angular Build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { IndexComponent } from './index/index.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GithubService } from './github.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserpageComponent } from './userpage/userpage.component';
+import { ReposComponent } from './repos/repos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ProfileComponent,
+    NavbarComponent,
+    UserpageComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: IndexComponent},
+      {path: ':username', component: UserpageComponent}
+    ])
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
